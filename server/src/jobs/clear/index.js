b3442e7a9f9c3dfee908c7182cd0e99c34492cb0
Antoine Bigard <6003532+antoinebigard@users.noreply.github.com>
2022-01-21 09:24:33 +0100

    refactor(technique): cleanup starts jobs: clear, seed, imports (#142)

--- server/src/jobs/clear/index.js
+++ server/src/jobs/clear/index.js
@@ -1,6 +1,61 @@
 const { runScript } = require("../scriptWrapper");
-const clearData = require("./clear");
+const {
+  User,
+  JwtSession,
+  MaintenanceMessage,
+  Role,
+  Workspace,
+  History,
+  Commentaire,
+  Permission,
+  ConnectionDossier,
+  CategoriesJuridique,
+  DdetsDreets,
+} = require("../../common/model/index");
+const { asyncForEach } = require("../../common/utils/asyncUtils");
 
-runScript(async () => {
-  await clearData();
+const clearDataImported = async () => {
+  await DdetsDreets.deleteMany({});
+  await CategoriesJuridique.deleteMany({});
+};
+
+const clearSessionsRelated = async () => {
+  await JwtSession.deleteMany({});
+  await ConnectionDossier.deleteMany({});
+};
+
+const clearUserRelated = async () => {
+  await Role.deleteMany({});
+  await User.deleteMany({});
+  await Workspace.deleteMany({});
+};
+
+const clearDossierRelated = async ({ dossiers }) => {
+  const dossiersDb = await dossiers.findDossiers({}, { _id: 1 });
+
+  await asyncForEach(dossiersDb, async ({ _id }) => {
+    await dossiers.removeDossier(_id);
+  });
+
+  await History.deleteMany({});
+  await Commentaire.deleteMany({});
+};
+
+runScript(async ({ dossiers }) => {
+  const args = process.argv.slice(2);
+  const clearAll = args.includes("--all");
+
+  if (clearAll) {
+    await clearDossierRelated({ dossiers });
+
+    await MaintenanceMessage.deleteMany({});
+    await clearUserRelated();
+    await Permission.deleteMany({});
+    await clearSessionsRelated();
+    await clearDataImported();
+  } else {
+    await clearDossierRelated({ dossiers });
+    await clearSessionsRelated();
+    await clearDataImported();
+  }
 });
--- server/src/jobs/seed/index.js
+++ server/src/jobs/seed/index.js
@@ -2,9 +2,89 @@ const { runScript } = require("../scriptWrapper");
 const logger = require("../../common/logger");
 const { MaintenanceMessage, Role, Cerfa } = require("../../common/model/index");
 
-const defaultRolesAcls = require("./defaultRolesAcls");
+const defaultRolesAcls = require("./fixtures/defaultRolesAcls");
+const cerfaData = require("./fixtures/cerfa.fixture");
+
+const createUsersPenTest = async ({ users }) => {
+  await users.createUser("contrat-pentest+admin1@apprentissage.beta.gouv.fr", "password", {
+    nom: "admin",
+    prenom: "1",
+    permissions: { isAdmin: true },
+    confirmed: true,
+  });
+  await users.createUser("contrat-pentest+admin2@apprentissage.beta.gouv.fr", "password", {
+    nom: "admin",
+    prenom: "2",
+    permissions: { isAdmin: true },
+    confirmed: true,
+  });
+  logger.info(`User '1admin' with password 'password' and admin is successfully created `);
+
+  await users.createUser("contrat-pentest+support1@apprentissage.beta.gouv.fr", "password", {
+    nom: "support",
+    prenom: "1",
+    roles: ["support"],
+    confirmed: true,
+  });
+  await users.createUser("contrat-pentest+support2@apprentissage.beta.gouv.fr", "password", {
+    nom: "support",
+    prenom: "2",
+    roles: ["support"],
+    confirmed: true,
+  });
+
+  await users.createUser("contrat-pentest+entreprise1@apprentissage.beta.gouv.fr", "password", {
+    nom: "entreprise",
+    prenom: "1",
+    roles: ["entreprise"],
+    confirmed: true,
+  });
+  await users.createUser("contrat-pentest+entreprise2@apprentissage.beta.gouv.fr", "password", {
+    nom: "entreprise",
+    prenom: "2",
+    roles: ["entreprise"],
+    confirmed: true,
+  });
+  const userCfa = await users.createUser("contrat-pentest+cfa1@apprentissage.beta.gouv.fr", "password", {
+    nom: "cfa",
+    prenom: "1",
+    roles: ["cfa"],
+    confirmed: true,
+  });
+  await users.createUser("contrat-pentest+cfa2@apprentissage.beta.gouv.fr", "password", {
+    nom: "cfa",
+    prenom: "2",
+    roles: ["cfa"],
+    confirmed: true,
+  });
+  logger.info(`User '1entreprise' with password 'password' is successfully created `);
+
+  return { userWks: userCfa, shareWithEmail: "contrat-pentest+entreprise1@apprentissage.beta.gouv.fr" };
+};
+
+const createUsersTest = async ({ users }) => {
+  const userAdmin = await users.createUser("antoine.bigard+testAdmin@beta.gouv.fr", "password", {
+    nom: "Admin",
+    prenom: "test",
+    permissions: { isAdmin: true },
+    confirmed: true,
+  });
+  logger.info(`User 'tAdmin' with password 'password' and admin is successfully created `);
+
+  await users.createUser("antoine.bigard+testEntreprise@beta.gouv.fr", "password", {
+    nom: "Damien",
+    prenom: "Arthur",
+    roles: ["entreprise"],
+    confirmed: true,
+  });
+  logger.info(`User 'tEntreprise' with password 'password' is successfully created `);
+  return { userWks: userAdmin, shareWithEmail: "antoine.bigard+testEntreprise@beta.gouv.fr" };
+};
 
 runScript(async ({ users, workspaces, dossiers }) => {
+  const args = process.argv.slice(2);
+  const seedPentest = args.includes("--pentest");
+
   await MaintenanceMessage.create({
     context: "automatique",
     type: "alert",
@@ -21,180 +101,24 @@ runScript(async ({ users, workspaces, dossiers }) => {
     logger.info(`Role ${key} created`);
   }
 
-  const userAdmin = await users.createUser("antoine.bigard+testAdmin@beta.gouv.fr", "password", {
-    nom: "Admin",
-    prenom: "test",
-    permissions: { isAdmin: true },
-    confirmed: true,
-  });
-  logger.info(`User 'tAdmin' with password 'password' and admin is successfully created `);
+  let createUsers = null;
+  if (seedPentest) {
+    createUsers = createUsersPenTest;
+  } else {
+    createUsers = createUsersTest;
+  }
 
-  await users.createUser("antoine.bigard+testEntreprise@beta.gouv.fr", "password", {
-    nom: "Damien",
-    prenom: "Arthur",
-    roles: ["entreprise"],
-    confirmed: true,
-  });
-  logger.info(`User 'tEntreprise' with password 'password' is successfully created `);
+  const { userWks, shareWithEmail } = await createUsers({ users });
 
-  const wks = await workspaces.getUserWorkspace(userAdmin, { _id: 1 });
+  const wks = await workspaces.getUserWorkspace(userWks, { _id: 1 });
+  await workspaces.addContributeur(wks._id, shareWithEmail, "wks.member");
+  logger.info(`Contributor added`);
 
-  await workspaces.addContributeur(wks._id, "antoine.bigard+testEntreprise@beta.gouv.fr", "wks.member");
-
-  const dossier = await dossiers.createDossier({ sub: userAdmin.email }, { nom: "Dossier Test", saved: true });
-
+  const dossier = await dossiers.createDossier({ sub: userWks.email }, { nom: "Dossier Test", saved: true });
   logger.info(`Dossier test created`);
 
   await Cerfa.create({
-    draft: true,
-    employeur: {
-      denomination: "SERVICE MEDICAL 83 SITE FREJUS",
-      siret: "18003502403698",
-      naf: "1031Z",
-      nombreDeSalaries: 123,
-      codeIdcc: "0043",
-      libelleIdcc:
-        "Convention collective nationale des entreprises de commission, de courtage et de commerce intracommunautaire et d'importation-exportation de France métropolitaine",
-      telephone: "0908070605",
-      courriel: "energie3000.pro@gmail.com",
-      adresse: {
-        numero: 435,
-        voie: "AV MAL DE LATTRE DE TASSIGNY",
-        complement: "IMMEUBLE LE SAGITTAIRE",
-        label: "435 AV MAL DE LATTRE DE TASSIGNY",
-        codePostal: "83600",
-        commune: "Fréjus",
-        departement: "33",
-        region: "75",
-      },
-      nom: "LEFEVBRE",
-      prenom: "MARTINE",
-      typeEmployeur: 11,
-      caisseComplementaire: "AGIRC-ARRCO",
-      regimeSpecifique: false,
-      attestationEligibilite: false,
-      attestationPieces: false,
-      privePublic: true,
-    },
-    apprenti: {
-      nom: "MARTIN",
-      prenom: "Jean-François",
-      sexe: "M",
-      nationalite: 1,
-      dateNaissance: "2001-01-01T00:00:00+0000",
-      departementNaissance: "2B",
-      communeNaissance: "Bourg-en-Bresse",
-      nir: "101010100100153",
-      regimeSocial: 1,
-      handicap: false,
-      situationAvantContrat: 1,
-      diplome: 26,
-      derniereClasse: 12,
-      diplomePrepare: 72,
-      intituleDiplomePrepare: "Master en sciences de l'éducation",
-      telephone: "0102030405",
-      courriel: "jf.martin@orange.fr",
-      adresse: {
-        numero: 20,
-        voie: "Boulevard de la liberté",
-        complement: "Etage 6 - Appartement 654",
-        label: "20 Boulevard de la liberté",
-        codePostal: "75000",
-        commune: "PARIS",
-        pays: "FRANCE",
-      },
-      responsableLegal: {
-        nom: "Honore",
-        prenom: "Robert",
-        adresse: {
-          numero: 20,
-          voie: "Boulevard de la liberté",
-          complement: "Etage 6 - Appartement 654",
-          label: "20 Boulevard de la liberté",
-          codePostal: "75000",
-          commune: "PARIS",
-          pays: "FRANCE",
-        },
-      },
-      inscriptionSportifDeHautNiveau: false,
-    },
-    maitre1: { nom: "Dupont", prenom: "Claire", dateNaissance: "1988-02-02T00:00:00+00:00" },
-    maitre2: { nom: "Dupont", prenom: "Claire", dateNaissance: "1988-02-02T00:00:00+00:00" },
-    formation: {
-      rncp: "RNCP15516",
-      codeDiplome: "32322111",
-      typeDiplome: 13,
-      intituleQualification: "Analyses agricoles biologiques et biotechnologiques",
-      dateDebutFormation: "2021-05-04T21:13:43+00:00",
-      dateFinFormation: "2021-05-04T21:13:43+00:00",
-      dureeFormation: 0,
-      dateObtentionDiplome: "2021-05-04T21:13:43+00:00",
-    },
-    contrat: {
-      modeContractuel: 1,
-      typeContratApp: 11,
-      numeroContratPrecedent: "02B202212000000",
-      noContrat: "02B202212000000",
-      noAvenant: "02B202212000000",
-      dateDebutContrat: "2021-02-01T00:00:00+00:00",
-      dateEffetAvenant: "2021-03-01T00:00:00+00:00",
-      dateConclusion: "2021-01-15T00:00:00+00:00",
-      dateFinContrat: "2021-02-28T00:00:00+00:00",
-      dateRupture: "2021-02-28T00:00:00+00:00",
-      lieuSignatureContrat: "PARIS",
-      typeDerogation: 11,
-      dureeTravailHebdoHeures: 37,
-      dureeTravailHebdoMinutes: 30,
-      travailRisque: true,
-      salaireEmbauche: 1530.45,
-      avantageNourriture: 3.55,
-      avantageLogement: 456.78,
-      autreAvantageEnNature: true,
-      remunerationsAnnuelles: [
-        {
-          dateDebut: "2021-02-01T00:00:00+00:00",
-          dateFin: "2021-02-28T00:00:00+00:00",
-          taux: 75.5,
-          tauxMinimal: 57,
-          salaireBrut: 1500,
-          typeSalaire: "SMIC",
-          ordre: "1.1",
-        },
-        {
-          dateDebut: "2021-02-01T00:00:00+00:00",
-          dateFin: "2021-02-28T00:00:00+00:00",
-          taux: 30.5,
-          tauxMinimal: 27,
-          salaireBrut: 1500,
-          typeSalaire: "SMC",
-          ordre: "2.1",
-        },
-        {
-          dateDebut: "2021-02-01T00:00:00+00:00",
-          dateFin: "2021-02-28T00:00:00+00:00",
-          taux: 75.5,
-          tauxMinimal: 57,
-          salaireBrut: 1500,
-          typeSalaire: "SMIC",
-          ordre: "3.1",
-        },
-      ],
-    },
-    organismeFormation: {
-      siret: "30291412200015",
-      denomination: "ASS DES MAISONS FAMILIALES",
-      formationInterne: false,
-      uaiCfa: "0123456A",
-      visaCfa: false,
-      adresse: {
-        numero: 5,
-        voie: "PL DU GENERAL DE GAULLE",
-        complement: "Etage 6 - Appartement 654",
-        label: "5 PL DU GENERAL DE GAULLE",
-        codePostal: "60380",
-        commune: "SONGEONS",
-      },
-    },
+    ...cerfaData,
     dossierId: dossier._id,
   });
   logger.info(`Seed cerfa created`);
