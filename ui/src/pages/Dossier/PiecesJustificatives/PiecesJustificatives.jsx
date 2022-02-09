2111ffde866d4b70cf1f6cb8ce0c0b0259c7056c
Pablo <69269013+pabloHry@users.noreply.github.com>
2022-02-09 13:58:13 +0100

    feat(ui): faire apparaître la mention sur la conservation des données (#315)

--- ui/src/pages/Dossier/PiecesJustificatives/PiecesJustificatives.jsx
+++ ui/src/pages/Dossier/PiecesJustificatives/PiecesJustificatives.jsx
@@ -39,6 +39,13 @@ export default () => {
             mt="2"
             onSubmittedField={onSubmittedEmployeurAttestationPieces}
           />
+          <Text color="grey.500" fontStyle="italic" fontSize="0.81rem">
+            Pendant la durée du contrat d’apprentissage, et après son terme, il peut vous être demandé de fournir
+            l’original du contrat signé, les pièces permettant d’attester du <br />
+            respect des déclarations figurant dans le contrat d’apprentissage ainsi que la convention de formation, et
+            le cas échéant la convention tripartite.
+            <br /> Il vous appartient donc de conserver l’ensemble de ces documents originaux.
+          </Text>
         </>
       )}
     </Box>
