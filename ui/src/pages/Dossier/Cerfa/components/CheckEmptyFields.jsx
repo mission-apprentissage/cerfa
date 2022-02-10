e34c77a73a85c0520c5f74d0e4e70ed382f67eb0
antoinebigard <bigard.antoine@gmail.com>
2022-02-10 14:36:57 +0100

    feat: rubans cosmétique

--- ui/src/pages/Dossier/Cerfa/components/CheckEmptyFields.jsx
+++ ui/src/pages/Dossier/Cerfa/components/CheckEmptyFields.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { Box, Button, Collapse, Text, List, ListItem, ListIcon, Link } from "@chakra-ui/react";
+import { Box, Button, Collapse, Text, List, ListItem, ListIcon, Link, Flex } from "@chakra-ui/react";
 import Ribbons from "../../../../common/components/Ribbons";
-import { ArrowRightLine } from "../../../../theme/components/icons";
+import { ArrowRightLine, ErrorIcon } from "../../../../theme/components/icons";
 // import { NavLink } from "react-router-dom";
 
 const CheckEmptyFields = ({ validate, fieldsErrored }) => {
   const [triggered, setTriggered] = useState(false);
-  console.log(fieldsErrored);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <Box mt={10}>
       <Button
@@ -15,66 +16,71 @@ const CheckEmptyFields = ({ validate, fieldsErrored }) => {
         variant="secondary"
         onClick={() => {
           setTriggered(true);
-          validate();
+          setIsLoading(true);
+
+          setTimeout(() => {
+            validate();
+            setIsLoading(false);
+          }, 220);
         }}
       >
         Est-ce que tous mes champs sont remplis ?
       </Button>
-      <Ribbons variant="success" mt={5}>
-        <Text>Tous les champs sont remplis</Text>
-      </Ribbons>
-      <Ribbons variant="error" mt={5}>
-        <Text>Tous les champs sont remplis</Text>
-      </Ribbons>
-      <Ribbons variant="warning" mt={5}>
-        <Text>Tous les champs sont remplis</Text>
-      </Ribbons>
-      <Ribbons variant="info" mt={5}>
-        <Text>Tous les champs sont remplis</Text>
-      </Ribbons>
       <Collapse in={triggered} animateOpacity>
-        {fieldsErrored.length > 0 && (
-          <Ribbons variant="error" mt={5}>
-            <Box>
-              <Text>{fieldsErrored.length} champ(s) non remplis :</Text>
-            </Box>
-            <List spacing={3} mt={3}>
-              {fieldsErrored.map(({ type, name, label }) => {
-                let anchor = `${name}_section-label`;
-                if (
-                  type === "text" ||
-                  type === "select" ||
-                  type === "radio" ||
-                  type === "phone" ||
-                  type === "email" ||
-                  type === "date"
-                ) {
-                  anchor = `${name}_section-label`;
-                }
-                return (
-                  <ListItem key={name}>
-                    <ListIcon as={ArrowRightLine} color="flaterror" />
-                    {/*
+        <Ribbons
+          variant={isLoading ? "unstyled" : fieldsErrored.length === 0 ? "success" : "error"}
+          mt={5}
+          oneLiner={!(fieldsErrored.length > 0 && !isLoading)}
+        >
+          {isLoading && <Text> Vérifications en cours...</Text>}
+          {fieldsErrored.length === 0 && !isLoading && <Text>Tous les champs sont remplis</Text>}
+          {fieldsErrored.length > 0 && !isLoading && (
+            <>
+              <Flex w="full" ml={10}>
+                <Text>
+                  <ErrorIcon boxSize="4" color="flaterror" mt="-0.125rem" mr={2} />
+                  {fieldsErrored.length} champ(s) non remplis :
+                </Text>
+              </Flex>
+              <List spacing={3} mt={3} ml={5}>
+                {fieldsErrored.map(({ type, name, label }) => {
+                  let anchor = `${name}_section-label`;
+                  if (
+                    type === "text" ||
+                    type === "select" ||
+                    type === "radio" ||
+                    type === "phone" ||
+                    type === "email" ||
+                    type === "date"
+                  ) {
+                    anchor = `${name}_section-label`;
+                  }
+                  return (
+                    <ListItem key={name}>
+                      <ListIcon as={ArrowRightLine} color="flaterror" />
+                      {/*
               TODO SHOULD BE LIKE THIS OR add archor dynanicly with react router
                <Link as={NavLink} to={"#apprenti_departementNaissance_section-label"}>
             Département de naissance
           </Link> */}
-                    <Link
-                      onClick={() => {
-                        const element = document.getElementById(anchor);
-                        if (element) {
-                          element.scrollIntoView({ behavior: "smooth" });
-                        }
-                      }}
-                    >
-                      {label.replace(":", "")}
-                    </Link>
-                  </ListItem>
-                );
-              })}
-            </List>
-          </Ribbons>
-        )}
+                      <Link
+                        onClick={() => {
+                          const element = document.getElementById(anchor);
+                          if (element) {
+                            element.scrollIntoView({ behavior: "smooth" });
+                          }
+                        }}
+                        textDecoration="underline"
+                      >
+                        {label.replace(":", "")}
+                      </Link>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </>
+          )}
+        </Ribbons>
       </Collapse>
     </Box>
   );
