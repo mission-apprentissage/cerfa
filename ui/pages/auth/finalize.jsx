50b3fa8093a21e314b4c32238a093ba0bcaa31ff
antoinebigard <bigard.antoine@gmail.com>
2022-03-28 15:12:45 +0200

    chore: wip

--- ui/pages/auth/finalize.jsx
+++ ui/pages/auth/finalize.jsx
@@ -20,6 +20,8 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import NavLink from "next/link";
 import * as Yup from "yup";
+import Head from "next/head";
+import { Page } from "../../components/Page/Page";
 
 import jwt from "jsonwebtoken";
 
@@ -125,136 +127,142 @@ const Finalize = () => {
   };
 
   return (
-    <Flex w="full" flexDirection="column" h="full" mt={4}>
-      <Box>
-        <Box marginBottom="2w">
-          {step === 0 && (
-            <FormControl py={2} isRequired isInvalid={errors.compte && touched.compte}>
-              <FormLabel>Je représente :</FormLabel>
-              <RadioGroup id="compte" name="compte" value={values.compte} mt={8}>
-                <VStack alignItems="baseline" fontSize="1.2rem" spacing={8}>
-                  <Radio
-                    value="entreprise"
-                    onChange={(e) => {
-                      setStep(1);
-                      handleChange(e);
-                      siretLookUp({ target: { value: values.siret } });
-                    }}
-                    size="lg"
-                  >
-                    une entreprise
-                  </Radio>
-                  <Radio
-                    value="cfa"
-                    onChange={(e) => {
-                      setStep(1);
-                      handleChange(e);
-                      siretLookUp({ target: { value: values.siret } });
-                    }}
-                    size="lg"
-                  >
-                    un CFA
-                  </Radio>
-                </VStack>
-              </RadioGroup>
-              {errors.compte && touched.compte && <FormErrorMessage>{errors.compte}</FormErrorMessage>}
-            </FormControl>
-          )}
-
-          {step === 1 && (
-            <>
-              <FormControl py={2} isRequired isInvalid={errors.siret && touched.siret}>
-                <FormLabel>Vérification de votre Siret</FormLabel>
-                <Input
-                  id="siret"
-                  name="siret"
-                  placeholder="Exemple 98765432400019"
-                  onChange={siretLookUp}
-                  value={values.siret}
-                  isDisabled={isFetching}
-                />
-                {errors.siret && touched.siret && <FormErrorMessage>{errors.siret}</FormErrorMessage>}
+    <Page>
+      <Head>
+        <title>Finalisation de votre inscription</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Flex w="full" flexDirection="column" h="full" mt={4}>
+        <Box>
+          <Box marginBottom="2w">
+            {step === 0 && (
+              <FormControl py={2} isRequired isInvalid={errors.compte && touched.compte}>
+                <FormLabel>Je représente :</FormLabel>
+                <RadioGroup id="compte" name="compte" value={values.compte} mt={8}>
+                  <VStack alignItems="baseline" fontSize="1.2rem" spacing={8}>
+                    <Radio
+                      value="entreprise"
+                      onChange={(e) => {
+                        setStep(1);
+                        handleChange(e);
+                        siretLookUp({ target: { value: values.siret } });
+                      }}
+                      size="lg"
+                    >
+                      une entreprise
+                    </Radio>
+                    <Radio
+                      value="cfa"
+                      onChange={(e) => {
+                        setStep(1);
+                        handleChange(e);
+                        siretLookUp({ target: { value: values.siret } });
+                      }}
+                      size="lg"
+                    >
+                      un CFA
+                    </Radio>
+                  </VStack>
+                </RadioGroup>
+                {errors.compte && touched.compte && <FormErrorMessage>{errors.compte}</FormErrorMessage>}
               </FormControl>
-              <Center
-                borderWidth="2px"
-                borderStyle="dashed"
-                borderColor={entrepriseData ? (entrepriseData.successed ? "green.500" : "error") : "grey.400"}
-                rounded="md"
-                minH="50"
-                flexDirection="column"
-                p={4}
-              >
-                {isFetching && <Spinner />}
-                {!isFetching && entrepriseData && (
-                  <>
-                    {entrepriseData.data && (
-                      <>
-                        {!entrepriseData.data.secretSiret && (
-                          <>
-                            <Box>{entrepriseData.data.enseigne || entrepriseData.data.entreprise_raison_sociale}</Box>
-                            <Box>
-                              {entrepriseData.data.numero_voie} {entrepriseData.data.nom_voie}
-                            </Box>
-                            {entrepriseData.data.complement_adresse && (
-                              <Box>{entrepriseData.data.complement_adresse}</Box>
-                            )}
-                            <Box>
-                              {entrepriseData.data.code_postal} {entrepriseData.data.localite}
-                            </Box>
-                          </>
-                        )}
-                        {entrepriseData.data.secretSiret && (
-                          <>
-                            <Box>Votre siret est valide.</Box>
-                            <Box>
-                              En revanche, en raison de sa nature, nous ne pourrons pas récupérer les informations
-                              reliées. (telles que l&apos;adresse et autres données)
-                            </Box>
-                          </>
-                        )}
-                      </>
-                    )}
-                    {entrepriseData.message && (
-                      <Box color="error" my={2}>
-                        {entrepriseData.message}
-                      </Box>
-                    )}
-                  </>
+            )}
+
+            {step === 1 && (
+              <>
+                <FormControl py={2} isRequired isInvalid={errors.siret && touched.siret}>
+                  <FormLabel>Vérification de votre Siret</FormLabel>
+                  <Input
+                    id="siret"
+                    name="siret"
+                    placeholder="Exemple 98765432400019"
+                    onChange={siretLookUp}
+                    value={values.siret}
+                    isDisabled={isFetching}
+                  />
+                  {errors.siret && touched.siret && <FormErrorMessage>{errors.siret}</FormErrorMessage>}
+                </FormControl>
+                <Center
+                  borderWidth="2px"
+                  borderStyle="dashed"
+                  borderColor={entrepriseData ? (entrepriseData.successed ? "green.500" : "error") : "grey.400"}
+                  rounded="md"
+                  minH="50"
+                  flexDirection="column"
+                  p={4}
+                >
+                  {isFetching && <Spinner />}
+                  {!isFetching && entrepriseData && (
+                    <>
+                      {entrepriseData.data && (
+                        <>
+                          {!entrepriseData.data.secretSiret && (
+                            <>
+                              <Box>{entrepriseData.data.enseigne || entrepriseData.data.entreprise_raison_sociale}</Box>
+                              <Box>
+                                {entrepriseData.data.numero_voie} {entrepriseData.data.nom_voie}
+                              </Box>
+                              {entrepriseData.data.complement_adresse && (
+                                <Box>{entrepriseData.data.complement_adresse}</Box>
+                              )}
+                              <Box>
+                                {entrepriseData.data.code_postal} {entrepriseData.data.localite}
+                              </Box>
+                            </>
+                          )}
+                          {entrepriseData.data.secretSiret && (
+                            <>
+                              <Box>Votre siret est valide.</Box>
+                              <Box>
+                                En revanche, en raison de sa nature, nous ne pourrons pas récupérer les informations
+                                reliées. (telles que l&apos;adresse et autres données)
+                              </Box>
+                            </>
+                          )}
+                        </>
+                      )}
+                      {entrepriseData.message && (
+                        <Box color="error" my={2}>
+                          {entrepriseData.message}
+                        </Box>
+                      )}
+                    </>
+                  )}
+                </Center>
+              </>
+            )}
+          </Box>
+          {step > 0 && (
+            <>
+              <HStack spacing="4w">
+                <Link onClick={() => setStep(step - 1)} color="grey.600">
+                  {"< Précedent"}
+                </Link>
+                {step === 1 && (
+                  <Button
+                    size="md"
+                    variant="primary"
+                    onClick={handleSubmit}
+                    px={6}
+                    isDisabled={!entrepriseData || !entrepriseData?.successed}
+                  >
+                    Finaliser votre inscription
+                  </Button>
                 )}
-              </Center>
+              </HStack>
+              <Flex flexGrow={1} alignItems="end">
+                <Text mt={8} fontSize="1rem">
+                  Vous rencontrez des difficultés à passer cette étape ?
+                  <Link href="/assistance" as={NavLink} color="bluefrance" ml={3}>
+                    Contacter l&apos;assistance
+                  </Link>
+                </Text>
+              </Flex>
             </>
           )}
         </Box>
-        {step > 0 && (
-          <>
-            <HStack spacing="4w">
-              <Link onClick={() => setStep(step - 1)} color="grey.600">
-                {"< Précedent"}
-              </Link>
-              {step === 1 && (
-                <Button
-                  size="md"
-                  variant="primary"
-                  onClick={handleSubmit}
-                  px={6}
-                  isDisabled={!entrepriseData || !entrepriseData?.successed}
-                >
-                  Finaliser votre inscription
-                </Button>
-              )}
-            </HStack>
-            <Flex flexGrow={1} alignItems="end">
-              <Text mt={8} fontSize="1rem">
-                Vous rencontrez des difficultés à passer cette étape ?
-                <Link href="/assistance" as={NavLink} color="bluefrance" ml={3}>
-                  Contacter l&apos;assistance
-                </Link>
-              </Text>
-            </Flex>
-          </>
-        )}
-      </Box>
-    </Flex>
+      </Flex>
+    </Page>
   );
 };
 
