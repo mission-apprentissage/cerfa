6e1bb68ef0c4dc99067d0efc9e2b682f24bb8581
antoinebigard <bigard.antoine@gmail.com>
2022-03-31 16:31:51 +0200

    chore: add worker

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
+  webpack: (config) => {
+    // load worker files as a urls by using Asset Modules
+    // https://webpack.js.org/guides/asset-modules/
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/worker/[hash][ext][query]",
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
