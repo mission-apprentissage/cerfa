535f6bdb8a0dfd82914c1a716c2672488c3f00a8
antoinebigard <bigard.antoine@gmail.com>
2022-04-21 08:29:30 +0200

    chore: wip

--- waf/99-fix-resolver.sh
+++ waf/99-fix-resolver.sh
@@ -8,10 +8,8 @@ RESOLVER_DIRECTIVE="resolver ${DNS_SERVER} ipv6=off valid=5s;"
 
 sed -i.bak "/http {/a ${RESOLVER_DIRECTIVE}" /etc/nginx/nginx.conf
 sed -i.bak "s/include includes\/location_common.conf;/include includes\/location_common.conf;\ninclude \/etc\/nginx\/conf.d\/ws.inc;/g" /etc/nginx/conf.d/default.conf
-sed -i.bak "s/ssl_dhparam /etc/ssl/certs/dhparam-2048.pem;/ssl_dhparam /ssl/ssl-dhparams.pem;/g" /etc/nginx/conf.d/default.conf
+# sed -i.bak "s/ssl_dhparam \/etc\/ssl\/certs\/dhparam-2048.pem;/ssl_dhparam \/ssl\/ssl-dhparams.pem;/g" /etc/nginx/conf.d/default.conf
 
 cat /etc/nginx/nginx.conf
 cat /etc/nginx/conf.d/default.conf
 
-
- 
