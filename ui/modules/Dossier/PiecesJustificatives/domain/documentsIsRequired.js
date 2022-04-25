f166c834878a04cf3646cf206094b377c2635539
flodlc <f.delacomble@gmail.com>
2022-04-25 12:08:36 +0200

    feat: documents obligatoires pour les avenants sauf 33 et 34

--- ui/modules/Dossier/PiecesJustificatives/domain/documentsIsRequired.js
+++ ui/modules/Dossier/PiecesJustificatives/domain/documentsIsRequired.js
@@ -1,1 +1,1 @@
-export const documentsIsRequired = (typeContratApp) => [11, 21, 22, 23, 33, 34, 35].includes(typeContratApp);
+export const documentsIsRequired = (typeContratApp) => [11, 21, 22, 23, 31, 32, 35, 36, 37].includes(typeContratApp);
