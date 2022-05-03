73d216a9e8eccfbe7209c0734c3905ffec489b0f
antoinebigard <bigard.antoine@gmail.com>
2022-05-03 10:47:14 +0200

    chore(release): revert sem release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const mainConfig = {
-  branches: ["main", "develop", { name: "next", channel: "beta", prerelease: "beta" }],
+  branches: ["main", { name: "develop", channel: "beta", prerelease: "beta" }],
   repositoryUrl: "https://github.com/mission-apprentissage/cerfa.git",
   plugins: [
     "@semantic-release/commit-analyzer",
@@ -53,8 +53,7 @@ const channel = createHash("md5").update(branch).digest("hex");
 const localConfig = {
   branches: [
     "main",
-    "develop",
-    { name: "next", channel: "beta", prerelease: "beta" },
+    { name: "develop", channel: "beta", prerelease: "beta" },
     {
       name: branch,
       channel,
