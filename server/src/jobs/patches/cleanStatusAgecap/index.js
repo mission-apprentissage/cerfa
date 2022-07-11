0bdf690998326e3e272ec12816cdb273ea3dc0c5
antoinebigard <bigard.antoine@gmail.com>
2022-07-11 09:24:41 +0200

    fix: synthax statut

--- server/src/jobs/patches/cleanStatusAgecap/index.js
+++ server/src/jobs/patches/cleanStatusAgecap/index.js
@@ -2,7 +2,14 @@ const { Dossier } = require("../../../common/model/index");
 
 const run = async () => {
   // On met tous les status_agecap à []
-  await Dossier.updateMany({}, { $set: { statusAgecap: [] } });
+  await Dossier.updateMany(
+    {},
+    {
+      $set: {
+        statutAgecap: [],
+      },
+    }
+  );
 };
 
 module.exports = run;
