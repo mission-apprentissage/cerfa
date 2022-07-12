44e9e297993930d1232b37d635ae68ca78963cc7
antoinebigard <bigard.antoine@gmail.com>
2022-07-12 11:00:32 +0200

    chore: wip putObject

--- server/src/common/utils/S3Utils.js
+++ server/src/common/utils/S3Utils.js
@@ -1,11 +1,30 @@
 const AWS = require("aws-sdk");
+// const config = require("../../config");
 
 const s3 = new AWS.S3();
+
+// const s3 = new AWS.S3({
+//   apiVersion: "2006-03-01",
+//   region: "eu-west-3",
+//   credentials: {
+//     accessKeyId: config.aws.AccessKeyId,
+//     secretAccessKey: config.aws.SecretAccessKey,
+//   },
+// });
 
 const BUCKET_NAME = "BUCKET";
 
 module.exports = {
   getS3ObjectAsStream: (key) => {
     return s3.getObject({ Bucket: BUCKET_NAME, Key: key }).createReadStream();
+  },
+  putS3ObjectAsStream: (fileStream, key) => {
+    return s3.putObject({
+      Bucket: BUCKET_NAME,
+      Key: key,
+      ACL: "public-read", // TODO Make it not Public read
+      Body: fileStream,
+      Metadata: { type: "pdf" },
+    });
   },
 };
