6e8029bed5001aa20bb76b6cc9c0464beeb5635a
Antoine Bigard <6003532+antoinebigard@users.noreply.github.com>
2022-07-18 16:55:08 +0200

    feat: ajout page de maintenance globale (#859)
    * feat: ajout du context Maintenance formulaire messages
    
    * feat: ajout page de maintenance

--- ui/components/Layout/Layout.jsx
+++ ui/components/Layout/Layout.jsx
@@ -6,13 +6,13 @@ import Header from "./components/Header";
 import NavigationMenu from "./components/NavigationMenu";
 import { HelpBox } from "../HelpBox/HelpBox";
 
-const Layout = ({ children, ...rest }) => {
+const Layout = ({ children, withoutDisplayNavigationBar = false, ...rest }) => {
   const router = useRouter();
   const isMyWorkspace = router.pathname.includes("/mes-dossiers");
   return (
     <Container maxW="full" minH="100vh" d="flex" flexDirection="column" p={0} {...rest}>
       <Header />
-      <NavigationMenu isMyWorkspace={isMyWorkspace} />
+      {!withoutDisplayNavigationBar && <NavigationMenu isMyWorkspace={isMyWorkspace} />}
       <Box minH={"60vh"} flexGrow="1">
         <Box w="100%" py={[4, 8]} px={[1, 1, 12, 24]}>
           <Container maxW="xl">{children}</Container>
